e2e15ee3e87b96281b1d121e9ba8aa1d471e2673
VietAnh26 <vietanhvu266@gmail.com>
2024-10-14 11:48:14 +0700

    Update App.py

--- App.py
+++ App.py
@@ -126,9 +126,11 @@ if st.session_state.active_button == 'button1':
             st.markdown(f'<span style="color:green; font-weight:bold;">{name}</span>', unsafe_allow_html=True)
             st.metric(f'{ck} stock', value = current/1000)
             if change >= 0:
+                st.write(change)
                 change = str(change)
                 st.markdown(f"<p style='color:green;'>+ {change}%</p>", unsafe_allow_html=True)
             else:
+                st.write(change)
                 change = str(change)
                 st.markdown(f"<p style='color:red;'>🔻{change}%</p>", unsafe_allow_html=True)
 
